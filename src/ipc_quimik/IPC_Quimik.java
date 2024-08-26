d632582a2fa7f6d0ece0527e630805efdec81a0e
Omar <galvezomar28@gmail.com>
2024-08-25 19:16:58 -0600

    Inicialización del Proyecto, creación de todas las ventanas

---
+++ src/ipc_quimik/IPC_Quimik.java
@@ -0,0 +1,15 @@
+
+package ipc_quimik;
+
+/**
+ *
+ * @author Omar
+ */
+public class IPC_Quimik {
+
+    
+    public static void main(String[] args) {
+        System.out.println("Bienvenido a IPC-Quimik");
+    }
+    
+}
